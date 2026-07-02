0d80cb22e2a3c7ee697d2448c28853eb39369459
กมลกานต์ บุญมาติ <kamonkarn2551@example.com>
2026-07-02 14:20:00 +0700

    first commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีครับ! ผม กมลกานต์ บูญมาติ\n");
+    printf("รหัสนักศึกษา: 692110122\n");
+    printf("วิชา: DG111 Computer Programming\n");
+
+    return 0;
+}
